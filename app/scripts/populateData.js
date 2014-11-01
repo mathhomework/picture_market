becac13649822b5e3420a2adde223faea55b3473
mathhomework <toxstalks@gmail.com>
2014-10-31 18:42:56 -0700

    bootstrap market page

--- app/scripts/populateData.js
+++ app/scripts/populateData.js
@@ -1,9 +1,10 @@
 var populateData = function(){
+    console.log('populating');
     var ref = new Firebase("https://piclistings.firebaseio.com");
     ref.set({
 
             blue: {
-                id: 42,
+                id: 1,
                 name: 'blue',
                 price: '1.00',
                 description: 'what more could you possibly want?',
@@ -12,7 +13,7 @@ var populateData = function(){
                 patrons: 'John'
             },
             red: {
-                id: 33,
+                id: 2,
                 name: 'red',
                 price: '1.25',
                 description: 'as red as a red square can be',
@@ -22,14 +23,51 @@ var populateData = function(){
 
             },
             green: {
-                id: 12,
+                id: 3,
                 name: 'green',
                 price: '1.50',
                 description: 'just like you',
                 hex: '#15FF00',
                 rgb: '0, 255, 0',
                 patrons: ''
+            },
+            orange:{
+                id: 4,
+                name:'orange',
+                price: '1.50',
+                description: 'just like you',
+                hex: '#FF8000',
+                rgb: '255, 128, 0',
+                patrons: ''
+            },
+            dark_purple:{
+                id: 5,
+                name:'dark purple',
+                price: '1.50',
+                description: 'royal',
+                hex: '#551A8B',
+                rgb: '85, 26, 139',
+                patrons: ''
+            },
+            yellow:{
+                id:6,
+                name:'yellow',
+                price:'0',
+                description:'best color',
+                hex: '#FFFF00',
+                rgb: '255,255,0',
+                patrons:''
+            },
+            peach:{
+                id:7,
+                name:'peach',
+                price:'2.50',
+                description:'yummy',
+                hex: '#FFDAB9',
+                rgb: '255,218,185',
+                patrons:''
             }
+
         }
     );
 };
